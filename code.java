82ccae53205f065d967452ba314a4e836ef552ad
Sarwer Hussain <48062618+hussainint@users.noreply.github.com>
2020-10-28 16:04:55 +0530

    Added condition till number 5

---
+++ code.java
@@ -0,0 +1,30 @@
+ package Dummy_Odd_Even;
+
+import java.util.Scanner;
+
+/**
+  * code
+  */
+ public class code {
+ 
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter a number to check for odd even Range(1-5)");
+        int k= sc.nextInt();
+
+        if(k == 1)
+        System.out.println("It is Odd");
+       else if(k == 2)
+        System.out.println("It is even");
+        else if(k == 3)
+        System.out.println("It is Odd");
+        else if(k == 4)
+        System.out.println("It is even");
+        else if(k == 5)
+        System.out.println("It is Odd");
+
+        sc.close();
+
+     }
+ }
